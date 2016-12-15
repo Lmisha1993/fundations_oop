ee5ae4026d85985d395ac683aa0ed91a87973ad9
Lmisha1993 <LMisha11993@gmail.com>
2016-12-15 23:59:56 +0200

    new

--- src/testing/Main.java
+++ src/testing/Main.java
@@ -2,15 +2,6 @@ package testing;
 
 public class Main {	
  public static void main(String[] args){
-//	Figure square = new Figure();
-//	square.setFigure("Square", "red");
-//	Draw s = new Square();
-//	s.Print(square.getName(), square.getColor());
-	
-//	Draw c = new Circle();
- //   Figure circle =new Ñ();
-  //  circle.setFigure("Circle", "green");
-//	c.Print(circle.getName(), circle.getColor());
 	
 	Figure square = new Square();
 	Figure circle = new Circle(); 
